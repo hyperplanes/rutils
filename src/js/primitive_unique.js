96407918dc8de14db7a00fe6ff67265b35f3ef1c
hyperplanes <16326475+hyperplanes@users.noreply.github.com>
2019-07-18 15:14:02 -0400

    Filter out nulls and undefineds, add support for NaNs in primitive_unique

--- src/js/primitive_unique.js
+++ src/js/primitive_unique.js
@@ -1,4 +1,11 @@
-export const primitive_unique=(A, B) => {
+export const primitive_unique=A => {
+	//R does not allow nulls in sequences, plus null.valueOf() is undefined. Use NaN instead
+	A=A.filter(x=>x!==null && x!==undefined); 
+	let indexOfNaN=A.findIndex(x=>typeof x==='number' && isNaN(x));
     let values = A.map(x=>x.valueOf());
-    return A.filter((x, i, arr) =>values.indexOf(x.valueOf()) === i);
+    let result=A.filter((x, i, arr) =>values.indexOf(x.valueOf()) === i);
+    if(indexOfNaN>=0){
+    	result.splice(indexOfNaN, 0, NaN);
+    }
+    return result;
 }
--- test/primitive_uniqueTests.js
+++ test/primitive_uniqueTests.js
@@ -1,1 +1,19 @@
 import {primitive_unique} from "../src/js/primitive_unique.js";
+QUnit.test( "primitive types", function( assert ) {
+	let symbol1=Symbol('foo');//symbols should always be considered unique by definition
+	let symbol2=Symbol('foo');
+	let A=[NaN,NaN,'foo','foo',1,'foo',1,BigInt(9007199254740991),BigInt(9007199254740991),2.2,2.2,-3,-3,null,null,undefined,undefined,,symbol1,symbol2,true,true,new Date(2010,1,1),new Date(2010,1,1),new Date(2010,1,1)];
+	let B=[NaN,'foo',1,BigInt(9007199254740991),2.2,-3,symbol1,symbol2,true,new Date(2010,1,1)];
+	assert.deepEqual(primitive_unique(A),B);
+});
+
+QUnit.test( "object types", function( assert ) {
+	let object1={
+		foo:'bar'
+	};
+	let object2={
+		foo:'bar'
+	};
+	let A=[object1,object1,object2,object2];
+	assert.deepEqual(primitive_unique(A),[object1,object2]);
+});

4117ebae0558a4b66b02aaaee590bf3d7e0befe3
hyperplanes <16326475+hyperplanes@users.noreply.github.com>
2019-07-19 10:44:13 -0400

    Refactor union to use spread operator instead of arguments parameter

--- test/identicalTests.js
+++ test/identicalTests.js
@@ -1,134 +1,134 @@
-import {deepEqual} from "../src/js/deepEqual.js";
+import {identical} from "../src/js/identical.js";
 
 QUnit.test( "two equal integers", function( assert ) {
 	let A=-1;
 	let B=-1;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two unequal integers", function( assert ) {
 	let A=-1;
 	let B=0;
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two equal strings", function( assert ) {
 	let A='foobar';
 	let B='foobar';
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two unequal strings", function( assert ) {
 	let A='foo';
 	let B='bar';
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "string and integer", function( assert ) {
 	let A=42;
 	let B='42';
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two equal bools", function( assert ) {
 	let A=true;
 	let B=true;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two false bools", function( assert ) {
 	let A=false;
 	let B=false;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two unequal bools", function( assert ) {
 	let A=true;
 	let B=false;
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "bool and string", function( assert ) {
 	let A=false;
 	let B='false';
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "bool and int", function( assert ) {
 	let A=false;
 	let B=0;
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two nulls", function( assert ) {
 	let A=null;
 	let B=null;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two undefined", function( assert ) {
 	let A;
 	let B;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "int and undefined", function( assert ) {
 	let A=1;
 	let B;
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two equal dates", function( assert ) {
 	let A=new Date(2019,1,1);
 	let B=new Date(2019,1,1);;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two unequal dates", function( assert ) {
 	let A=new Date(2019,1,2);
 	let B=new Date(2019,1,1);;
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two dates which are the same object", function( assert ) {
 	let A=new Date(2019,1,2);
 	let B=A;
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 
 QUnit.test( "two equal arrays", function( assert ) {
 	let A=[1,2,3];
 	let B=[1,2,3];
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two unequal arrays", function( assert ) {
 	let A=[1,2,3];
 	let B=[1,42,3];
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "two arrays in different orders", function( assert ) {
 	let A=[1,2,3];
 	let B=[1,3,2];
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two arrays of different lengths", function( assert ) {
 	let A=[1,2,3];
 	let B=[1,2,3,42];
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two equal sparse arrays", function( assert ) {
 	let A=[1,,3];
 	let B=[1,,3];
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two unequal sparse arrays", function( assert ) {
 	let A=[1,,3];
 	let B=[1,2,3];
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "arrays of nulls", function( assert ) {
 	let A=[null,null];
 	let B=[null,null];
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "arrays of nulls versus undefined", function( assert ) {
 	let A=[null,null];
 	let B=[null,undefined];
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "arrays of strings", function( assert ) {
 	let A=['a','b'];
 	let B='ab';
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "array versus array-like object", function( assert ) {
 	let A=['a','b'];
@@ -137,14 +137,14 @@ QUnit.test( "array versus array-like object", function( assert ) {
 		1:'b',
 		length:2
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "array versus array-like object2", function( assert ) {
 	let A=['a','b'];
 	let B={};
 	[].push.call(B,'a');
 	[].push.call(B,'b');
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "two objects that are the same but created differently", function( assert ) {
 	let A={
@@ -155,7 +155,7 @@ QUnit.test( "two objects that are the same but created differently", function( a
 	let B={};
 	[].push.call(B,'a');
 	[].push.call(B,'b');
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two objects that are the same", function( assert ) {
 	let A={
@@ -168,7 +168,7 @@ QUnit.test( "two objects that are the same", function( assert ) {
 		1:'b',
 		length:2
 	};
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "two objects that are different", function( assert ) {
 	let A={
@@ -181,7 +181,7 @@ QUnit.test( "two objects that are different", function( assert ) {
 		1:'b',
 		length:2
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "circular reference that can be solved", function( assert ) {
 	let A={
@@ -195,7 +195,7 @@ QUnit.test( "circular reference that can be solved", function( assert ) {
 		length:2,
 		2:A
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 QUnit.test( "circular reference that can be solved", function( assert ) {
 	let A={
@@ -208,7 +208,7 @@ QUnit.test( "circular reference that can be solved", function( assert ) {
 		1:A,
 		length:2
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "circular reference that can be solved 2", function( assert ) {
@@ -223,7 +223,7 @@ QUnit.test( "circular reference that can be solved 2", function( assert ) {
 		length:2,
 		2:A
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "circular reference that can be solved 3", function( assert ) {
@@ -239,7 +239,7 @@ QUnit.test( "circular reference that can be solved 3", function( assert ) {
 	};
 	[].push.call(A,A);
 	[].push.call(B,A);
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 
 QUnit.test( "circular reference that causes infinite loop", function( assert ) {
@@ -255,7 +255,7 @@ QUnit.test( "circular reference that causes infinite loop", function( assert ) {
 	[].push.call(B,A);
 	let r='no errors yet';
 	try{
-		deepEqual(A,B);
+		identical(A,B);
 	}catch(e){
 		r=e;
 	}
@@ -274,7 +274,7 @@ QUnit.test( "objects with arrays equal", function( assert ) {
 		1:'b',
 		2:['a','b']
 	};
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 
 QUnit.test( "objects with arrays unequal", function( assert ) {
@@ -288,7 +288,7 @@ QUnit.test( "objects with arrays unequal", function( assert ) {
 		1:'b',
 		2:['a','b','c']
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 
@@ -303,7 +303,7 @@ QUnit.test( "objects with nested arrays equal", function( assert ) {
 		1:'b',
 		2:['a','b',['a','b',['a','b']]]
 	};
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
 QUnit.test( "objects with nested arrays unequal", function( assert ) {
 	let A={
@@ -316,7 +316,7 @@ QUnit.test( "objects with nested arrays unequal", function( assert ) {
 		1:'b',
 		2:['a','b',['a','b',['a','b']]]
 	};
-	assert.equal(deepEqual(A,B),false);
+	assert.equal(identical(A,B),false);
 });
 
 QUnit.test( "objects with nested arrays and date equal", function( assert ) {
@@ -330,5 +330,5 @@ QUnit.test( "objects with nested arrays and date equal", function( assert ) {
 		1:'b',
 		2:['a','b',['a','b',['a','b',new Date(2010,1,1)]]]
 	};
-	assert.equal(deepEqual(A,B),true);
+	assert.equal(identical(A,B),true);
 });
--- test/unionTests.js
+++ test/unionTests.js
@@ -1,1 +1,16 @@
 import {union} from "../src/js/union.js";
+QUnit.test( "union", function( assert ) {
+	let object1={
+		foo:'bar'
+	};
+	let object2={
+		foo:'bar'
+	};
+	let symbol1=Symbol('foo');//symbols should always be considered unique by definition
+	let symbol2=Symbol('foo');
+	let A=[NaN,NaN,'foo','foo',1,'foo',1,BigInt(9007199254740991),BigInt(9007199254740991),2.2,2.2,-3,-3,null,null,undefined,undefined,,symbol1,symbol2,true,true,new Date(2010,1,1),new Date(2010,1,1),new Date(2010,1,1),object1,object1,object2,object2];
+	let B=[NaN,NaN,'bar','bar',2,'bar',1,BigInt(9007199254740991),BigInt(9007199254740991),2.2,2.2,-3,-3,null,null,undefined,undefined,,symbol1,symbol2,true,true,new Date(2010,1,1),new Date(2010,1,1),new Date(2010,1,1),object1,object1,object2,object2];
+	//union concatenates the argument arrays first, then applies unique
+	let C=[NaN,'foo',1,BigInt(9007199254740991),2.2,-3,symbol1,symbol2,true,new Date(2010,1,1),object1,'bar',2];
+	assert.deepEqual(union(A,B),C);
+});
